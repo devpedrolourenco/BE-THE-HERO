cfc79a32e76cf065c6012ad5d55aca42001d7f1f
Pedro Lourenço de Oliveira <55964391+devpedrolourenco@users.noreply.github.com>
2020-03-25 08:47:41 -0300

    refator. 0.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Header from './Header';
+
 
 
 function App() {
+ const [counter, setCounter] = useState(0)
+
+  function increment(){
+    setCounter(counter +1)
+
+    console.log(counter)
+  }
   return (
-    <h1>Hello World</h1>
+    <div>
+    <Header>Contador: {counter}</Header>
+    <button onClick={increment}>INCREMENTAR</button>
+    </div>
   );
 }
 
