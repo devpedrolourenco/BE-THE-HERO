667d9bf1e6b8a865c519442df5cf4b2d45adc439
Pedro Lourenço de Oliveira <55964391+devpedrolourenco@users.noreply.github.com>
2020-03-26 13:02:18 -0300

    att

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -50,7 +50,7 @@ export default function Detail() {
         <Text style={styles.incidentValue}>{incident.title}</Text>
 
         <Text style={styles.incidentProperty}>VALOR:</Text>
-        <Text style={styles.incidentValue}>{incident.value}</Text>
+        <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(incident.value)}</Text>
       </View>
 
       <View style={styles.contactBox}>
